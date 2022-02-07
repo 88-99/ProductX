401abc41598079ca9f9519261c21289fba31fb83
KW <kw@KWnoMacBook-Pro.local>
2022-02-07 19:22:59 +0900

    devise_create_usersのマイグレファイルにt.boolean :admin, default: falseを追記、create、migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,24 +15,6 @@ ActiveRecord::Schema.define(version: 2022_02_02_020329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "answers", force: :cascade do |t|
     t.text "content", null: false
     t.bigint "user_id", null: false
@@ -170,6 +152,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_020329) do
     t.string "name", null: false
     t.string "phone_number", null: false
     t.string "nickname", null: false
+    t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
