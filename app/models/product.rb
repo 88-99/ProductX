68b65abbae89fd3b9b31a5a44f0f576793949425
KW <kw@KWnoMacBook-Pro.local>
2022-02-12 00:05:55 +0900

    product.rbにvalidationを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,7 @@ class Product < ApplicationRecord
   validates :code, uniqueness: true, length: { maximum: 12 },
                    format: { with: /\A[a-zA-Z0-9ｱ-ﾝﾞﾟ]+\z/ }
   validates :name,  presence: true, length: { maximum: 30 }
+  validates :cost_price,  presence: true
+  validates :selling_price,  presence: true
   validates :remarks, length: { maximum: 800 }
 end
