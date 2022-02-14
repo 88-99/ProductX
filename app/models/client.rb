7ad68a92a8967d315e1f5cf90dfc74626715d407
KW <kw@KWnoMacBook-Pro.local>
2022-02-14 21:04:19 +0900

    seed.rbでclient, supplierのレコードを作成

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ApplicationRecord
   has_many :teams_clients
   has_many :teams, through: :teams_clients
   has_many :sales, dependent: :destroy
-  validates :name,  presence: true, length: { maximum: 20 }
+  validates :name,  presence: true, length: { maximum: 30 }
   validates :phone_number, length: { maximum: 20 }
   validates :fax_number, length: { maximum: 20 }
   validates :address, length: { maximum: 40 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,28 @@
 #                )
 # end
 
+# 5.times do |n|
+#   Product.create!(code: "product#{n+1}",
+#                name: "ppp-#{n+1}",
+#                regular_price: "#{n+300}",
+#                selling_price: "#{n+200}",
+#                cost_price: "#{n+100}",
+#                user_id: User.first.id
+#                )
+# end
+
+# 5.times do |n|
+#   name = Faker::Company.name
+#   Client.create!(name: name)
+# end
+
+5.times do |n|
+  name = Faker::Company.name
+  Supplier.create!(name: name)
+end
+
+
+
 # 1.times do |n|
 #   @user = User.create!(name: "seed_test",
 #                email: "email@email.com",
@@ -23,14 +45,3 @@
 #                admin: false
 #                )
 # end
-
-
-5.times do |n|
-  Product.create!(code: "product#{n+1}",
-               name: "ppp-#{n+1}",
-               regular_price: "#{n+300}",
-               selling_price: "#{n+200}",
-               cost_price: "#{n+100}",
-               user_id: User.first.id
-               )
-end
