a224b462b8e0d7e965afa9ab069299822253c0af
KW <kw@KWnoMacBook-Pro.local>
2022-02-15 15:25:51 +0900

    seedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,12 +28,85 @@
 #   Client.create!(name: name)
 # end
 
+# 5.times do |n|
+#   name = Faker::Company.name
+#   Supplier.create!(name: name)
+# end
+
+# 5.times do |n|
+#   Purchase.create!(
+#            user_id:     User.first.id,
+#            supplier_id: Supplier.first.id,
+#            date_at:     "2022020#{n+1}",
+#            inputter:    User.first.id
+#            )
+# end
+
+# 5.times do |n|
+#   Purchase.create!(
+#            user_id:     (User.first.id)+1,
+#            supplier_id: (Supplier.first.id)+1,
+#            date_at:     "2022020#{n+1}",
+#            inputter:    (User.first.id)+1
+#            )
+# end
+
+# 5.times do |n|
+#   PurchaseDetail.create!(
+#                  purchase_id: (Purchase.last.id)-n,
+#                  product_id:  (Product.last.id)-n,
+#                  quantity:    (n+1)*100
+#                  )
+# end
+
+# 5.times do |n|
+#   Sale.create!(
+#        user_id:     User.first.id,
+#        supplier_id: Supplier.first.id,
+#        date_at:     "2022020#{n+1}",
+#        inputter:    User.first.id
+#        )
+# end
+
+# 5.times do |n|
+#   Sale.create!(
+#        user_id:   (User.first.id)+1,
+#        client_id: (Client.first.id)+1,
+#        date_at:   "2022020#{n+1}",
+#        inputter:  (User.first.id)+1
+#        )
+# end
+
 5.times do |n|
-  name = Faker::Company.name
-  Supplier.create!(name: name)
+  SaleDetail.create!(
+  sale_id:     (Sale.last.id)-n,
+  product_id:  (Product.last.id)-n,
+  quantity:    (n+1)*100
+  )
 end
 
 
+# 1.times do |n|
+#   Purchase.create!({
+#   date_at:  "2022020#{n+3}",
+#   inputter:   User.first.id,
+#   purchase_details_attributes: [    
+        
+#           quantity:     "100",
+#           purchase_id:   Purchase.find.first,
+#           product_id:    product.find.first
+        
+#         # {
+#         #   title:   Faker::Lorem.sentence,
+#         #   content: Faker::Lorem.paragraph(200),
+#         # },
+#         # {
+#         #   title:   Faker::Lorem.sentence,
+#         #   content: Faker::Lorem.paragraph(200),
+#         # }    
+#     ]
+#   })
+# end
 
 # 1.times do |n|
 #   @user = User.create!(name: "seed_test",
