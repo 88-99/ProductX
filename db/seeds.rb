707cabd90a0f3adc28143060e2a89ad775296b74
KW <kw@KWnoMacBook-Pro.local>
2022-02-16 21:11:32 +0900

    seed.rbの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# 10.times do |n|
-#   name = Faker::FunnyName.name
-#   email = Faker::Internet.email
-#   phone_number = Faker::PhoneNumber.phone_number
-#   nickname = Faker::Games::Pokemon.name
-#   @user = User.create!(name: name,
-#                email: email,
-#                password: "aaaaaa",
-#                password_confirmation: "aaaaaa",
-#                phone_number: phone_number,
-#                nickname: nickname,               
-#                admin: false
-#                )
-# end
+5 .times do |n|
+  name = Faker::FunnyName.name
+  email = Faker::Internet.email
+  phone_number = Faker::PhoneNumber.phone_number
+  nickname = Faker::Games::Pokemon.name
+  @user = User.create!(name: name,
+               email: email,
+               password: "aaaaaa",
+               password_confirmation: "aaaaaa",
+               phone_number: phone_number,
+               nickname: nickname,               
+               admin: false
+               )
+end
 
 # 5.times do |n|
 #   Product.create!(code: "product#{n+1}",
@@ -84,96 +84,3 @@
 #   quantity:    (n+1)*100
 #   )
 # end
-
-
-# 1.times do |n|
-#   Purchase.create!({
-#   date_at:  "2022020#{n+3}",
-#   inputter:   User.first.id,
-#   purchase_details_attributes: [    
-        
-#           quantity:     "100",
-#           purchase_id:   Purchase.find.first,
-#           product_id:    product.find.first
-        
-#         # {
-#         #   title:   Faker::Lorem.sentence,
-#         #   content: Faker::Lorem.paragraph(200),
-#         # },
-#         # {
-#         #   title:   Faker::Lorem.sentence,
-#         #   content: Faker::Lorem.paragraph(200),
-#         # }    
-#     ]
-#   })
-# end
-
-
-
-
-# 1.times do |n|
-#   @user = User.create!(name: "aaa@aaa.com",
-#                email: "aaa@aaa.com",
-#                password: "aaaaaa",
-#                password_confirmation: "aaaaaa",
-#                phone_number: "aaa@aaa.com",
-#                nickname: "aaa@aaa.com",               
-#                admin: true
-#                )
-# end
-
-
-# 3.times do |n|
-#   Product.create!(code: "ppp#{n+1}",
-#                name: "ppp#{n+1}",
-#                regular_price: "#{(n+1)*300}",
-#                selling_price: "#{(n+1)*200}",
-#                cost_price: "#{(n+1)*100}",
-#                user_id: User.first.id
-#                )
-# end
-
-
-# 1.times do |n|
-#   Client.create!(name: "tk")
-# end
-
-# 1.times do |n|
-#   Supplier.create!(name: "si")
-# end
-
-
-# 2.times do |n|
-#   Purchase.create!(
-#            user_id:     User.first.id,
-#            supplier_id: Supplier.first.id,
-#            date_at:     "2022020#{n+1}",
-#            inputter:    User.first.id
-#            )
-# end
-
-# 2.times do |n|
-#   Sale.create!(
-#        user_id:   User.first.id,
-#        client_id: Client.first.id,
-#        date_at:   "2022020#{n+1}",
-#        inputter:  User.first.id
-#        )
-# end
-
-
-3.times do |n|
-  PurchaseDetail.create!(
-                 purchase_id: (Purchase.first.id),
-                 product_id:  (Product.last.id),
-                 quantity:    (n+1)*200
-                 )
-end
-
-3.times do |n|
-  SaleDetail.create!(
-                 sale_id: (Sale.first.id),
-                 product_id:  (Product.last.id),
-                 quantity:    (n+1)*100
-                 )
-end
