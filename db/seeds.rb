0dd05034c55e44e31a246e018720b3fbc5edbaac
KW <kw@KWnoMacBook-Pro.local>
2022-02-16 21:40:46 +0900

    seed.rbを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
-5 .times do |n|
-  name = Faker::FunnyName.name
-  email = Faker::Internet.email
-  phone_number = Faker::PhoneNumber.phone_number
-  nickname = Faker::Games::Pokemon.name
-  @user = User.create!(name: name,
-               email: email,
-               password: "aaaaaa",
-               password_confirmation: "aaaaaa",
-               phone_number: phone_number,
-               nickname: nickname,               
-               admin: false
-               )
-end
-
-# 5.times do |n|
-#   Product.create!(code: "product#{n+1}",
-#                name: "ppp-#{n+1}",
-#                regular_price: "#{n+300}",
-#                selling_price: "#{n+200}",
-#                cost_price: "#{n+100}",
-#                user_id: User.first.id
+# 5 .times do |n|
+#   name = Faker::FunnyName.name
+#   email = Faker::Internet.email
+#   phone_number = Faker::PhoneNumber.phone_number
+#   nickname = Faker::Games::Pokemon.name
+#   @user = User.create!(name: name,
+#                email: email,
+#                password: "aaaaaa",
+#                password_confirmation: "aaaaaa",
+#                phone_number: phone_number,
+#                nickname: nickname,               
+#                admin: false
 #                )
 # end
 
-# 5.times do |n|
-#   name = Faker::Company.name
-#   Client.create!(name: name)
-# end
+5.times do |n|
+  Product.create!(code: "ppp#{n+1}",
+               name: "ppp#{n+1}",
+               regular_price: "#{(n+1)*300}",
+               selling_price: "#{(n+1)*200}",
+               cost_price: "#{(n+1)*100}",
+               user_id: User.first.id
+               )
+end
 
-# 5.times do |n|
-#   name = Faker::Company.name
-#   Supplier.create!(name: name)
-# end
+5.times do |n|
+  name = Faker::Company.name
+  Client.create!(name: name)
+end
+
+5.times do |n|
+  name = Faker::Company.name
+  Supplier.create!(name: name)
+end
 
 # 5.times do |n|
 #   Purchase.create!(
