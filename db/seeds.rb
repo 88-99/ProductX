16706de7596db01edcced0c10e7a38a54c2878a6
KW <kw@KWnoMacBook-Pro.local>
2022-02-16 22:11:13 +0900

    seedデータの挿入 Purachase, Sale

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,43 +13,63 @@
 #                )
 # end
 
-5.times do |n|
-  Product.create!(code: "ppp#{n+1}",
-               name: "ppp#{n+1}",
-               regular_price: "#{(n+1)*300}",
-               selling_price: "#{(n+1)*200}",
-               cost_price: "#{(n+1)*100}",
-               user_id: User.first.id
-               )
-end
-
-5.times do |n|
-  name = Faker::Company.name
-  Client.create!(name: name)
-end
-
-5.times do |n|
-  name = Faker::Company.name
-  Supplier.create!(name: name)
-end
-
 # 5.times do |n|
-#   Purchase.create!(
-#            user_id:     User.first.id,
-#            supplier_id: Supplier.first.id,
-#            date_at:     "2022020#{n+1}",
-#            inputter:    User.first.id
-#            )
+#   Product.create!(code: "ppp#{n+1}",
+#                name: "ppp#{n+1}",
+#                regular_price: "#{(n+1)*300}",
+#                selling_price: "#{(n+1)*200}",
+#                cost_price: "#{(n+1)*100}",
+#                user_id: User.first.id
+#                )
 # end
 
 # 5.times do |n|
-#   Purchase.create!(
-#            user_id:     (User.first.id)+1,
-#            supplier_id: (Supplier.first.id)+1,
-#            date_at:     "2022020#{n+1}",
-#            inputter:    (User.first.id)+1
-#            )
+#   name = Faker::Company.name
+#   Client.create!(name: name)
 # end
+
+# 5.times do |n|
+#   name = Faker::Company.name
+#   Supplier.create!(name: name)
+# end
+
+
+5.times do |n|
+  Purchase.create!(
+           user_id:     User.first.id,
+           supplier_id: Supplier.first.id,
+           date_at:     "2022020#{n+1}",
+           inputter:    User.first.id
+           )
+end
+
+5.times do |n|
+  Purchase.create!(
+           user_id:     (User.first.id)+1,
+           supplier_id: (Supplier.first.id)+1,
+           date_at:     "2022020#{2*n+1}",
+           inputter:    (User.first.id)+1
+           )
+end
+
+5.times do |n|
+  Sale.create!(
+           user_id:   User.first.id,
+           client_id: Client.first.id,
+           date_at:   "2022020#{n+1}",
+           inputter:  User.first.id
+           )
+end
+
+5.times do |n|
+  Sale.create!(
+           user_id:   (User.first.id)+1,
+           client_id: (Client.first.id)+1,
+           date_at:   "2022020#{2*n+1}",
+           inputter:  (User.first.id)+1
+           )
+end
+
 
 # 5.times do |n|
 #   PurchaseDetail.create!(
@@ -57,24 +77,6 @@ end
 #                  product_id:  (Product.last.id)-n,
 #                  quantity:    (n+1)*100
 #                  )
-# end
-
-# 5.times do |n|
-#   Sale.create!(
-#        user_id:   User.first.id,
-#        client_id: Client.first.id,
-#        date_at:   "2022020#{n+1}",
-#        inputter:  User.first.id
-#        )
-# end
-
-# 5.times do |n|
-#   Sale.create!(
-#        user_id:   (User.first.id)+1,
-#        client_id: (Client.first.id)+1,
-#        date_at:   "2022020#{n+1}",
-#        inputter:  (User.first.id)+1
-#        )
 # end
 
 # 5.times do |n|
