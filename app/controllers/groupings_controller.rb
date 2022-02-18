95b568c727536121a20e01b43e161e78c23a6a60
KW <kw@KWnoMacBook-Pro.local>
2022-02-19 00:47:49 +0900

    GroupingsControllerを修正

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,33 +1,36 @@
 class GroupingsController < ApplicationController
-  before_action :set_grouping, only: %i[show] #destroy]
+  before_action :set_grouping, only: %i[show]
 
   def new
     @grouping = Grouping.new
   end
 
   def create
-    @team = Team.find(current_user.team.id) # params[team:]
-    unless Grouping.team_assign?(@user)
-        @user = User.find_by(email: params[:grouping][:email])
+    @team = Team.find(current_user.team.id)
+    @user = User.find_by(email: params[:grouping][:email])
+    unless @team.groupings.find_by(user_id: @user).present?
+      if @user.present?
         @grouping = @user.groupings.build(team_id: @team.id)
         @grouping.save
-        redirect_to team_path(@team.id), notice: "メンバーを登録しました！"     
+        redirect_to team_path(@team.id), notice: "メンバーを登録しました！"
+      else
+        redirect_to new_team_grouping_path(params[:team_id]), notice: "#{params[:grouping][:email]}はユーザ登録されていないので、メンバーに追加できません。"        
+      end      
     else
-      render :new
+      redirect_to new_team_grouping_path(params[:team_id]), notice: "#{@user.name}は既にメンバー登録されています。"      
     end
   end
 
-  def edit
-  end
-
   def destroy
-    @user = User.find(params[:id])
-    @team = Team.find(current_user.team.id)
-    # @team = Team.find(params[:id])
-    @grouping = @user.groupings.last
-    @grouping.destroy
-    # @grouping = @team.user.groupings.destroy
-    # redirect_to team_path.(@team.id), notice:"メンバーを削除しました！"
+    @user = User.find(params[:user_id])
+    @team = Team.find(params[:team_id])
+    @grouping = @team.groupings.find_by(user_id: @user)
+    unless @team.chief == @user.id
+      @grouping.destroy
+      redirect_to team_path(@team.id), notice:"メンバーを削除しました！"
+    else
+      redirect_to team_path(@team.id), notice:"チームのオーナーは削除できません。"
+    end
   end
 
   private
