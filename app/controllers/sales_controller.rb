411d23582cf29d66f7d4382196eee620001ca801
KW <kw@KWnoMacBook-Pro.local>
2022-02-20 20:16:13 +0900

    SalesControllerのdef editに＠products = ＠team.productsを追記

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -18,18 +18,19 @@ class SalesController < ApplicationController
     # @sale = Sale.new(sale_params)
     # @sale.user_id = current_user.id
     if @sale.save
-      redirect_to edit_sale_path(@sale.id), notice: "売上を登録しました！"
+      redirect_to edit_sale_path(@team, @sale.id), notice: "売上を登録しました！"
     else
       render :new
     end    
   end
   
   def edit
+    @products = @team.products
     @total_amount = 0
     @sale.sale_details.each {|detail| @total_amount += detail.quantity * detail.product.selling_price}
     
     @sale.sale_details.build
-    @products = Sale.find(params[:id]).saled_products
+    # @products = Sale.find(params[:id]).saled_products
   end
 
   def update    
