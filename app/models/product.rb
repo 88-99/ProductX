1bf0fb820a97464d181901c940975c4b532919c4
KW <kw@KWnoMacBook-Pro.local>
2022-02-25 23:46:15 +0900

    validationを変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,9 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :sale_details
   has_many :purchase_details
-  validates :code, uniqueness: { scope: :team_id }, length: { maximum: 12 },
+  validates :code, uniqueness: { scope: :team_id },
                    format: { with: /\A[a-zA-Z0-9ｱ-ﾝﾞﾟ]+\z/ }
-  validates :name,  presence: true, length: { maximum: 30 }
+  validates :name,  presence: true
   validates :cost_price,  presence: true
   validates :selling_price,  presence: true
-  validates :remarks, length: { maximum: 800 }
 end
