42a6c52b5962add3429cff5061a57ff8fee6e3e6
KW <kw@KWnoMacBook-Pro.local>
2022-02-26 19:15:03 +0900

    factorybot user にFakerを利用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,21 +1,21 @@
 FactoryBot.define do
   factory :user do
     trait :a do
-      id { 1 }
       name { "factory_user_aaa" }
-      email { "test@user.com" }
-      phone_number { "09011112222" }
-      nickname { "ニックネームaaa" }
+      email { Faker::Internet.free_email }
+      # sequence(:email) { |n| "tama#{n}@cat.com" }
+      phone_number { Faker::PhoneNumber.cell_phone_in_e164 }
+      nickname { Faker::Name.name }
       password { "aaaaaa" }
       admin { false }
     end
 
     trait :b do
-      id { 2 }
       name { "factory_user_bbb" }
-      email { "test@user2.com" }
-      phone_number { "09033334444" }
-      nickname { "ニックネーム_bbb" }
+      email { Faker::Internet.free_email }
+      # sequence(:email) { |n| "tama#{n}@cat.com" }
+      phone_number { Faker::PhoneNumber.cell_phone_in_e164 }
+      nickname { Faker::Name.name }
       password { "bbbbbb" }
       admin { false }
     end
