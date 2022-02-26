52c53e244ada8a9b70e76458b92fbaf9dbe6876b
KW <kw@KWnoMacBook-Pro.local>
2022-02-26 11:56:11 +0900

    context '仕入を新規登録した場合' doを編集中

--- spec/system/purchase_spec.rb
+++ spec/system/purchase_spec.rb
@@ -2,29 +2,30 @@
 # RSpec.describe '仕入登録機能', type: :system do
 
 #   before do
-#     user = FactoryBot.create(:user, :a)
-#     @team = FactoryBot.create(:team, user: user)
-#     @product = FactoryBot.create(:product, user: user, team: @team)
-#     @second_product = FactoryBot.create(:second_product, user: user, team: @team)
-#     visit new_user_session_path
-#     fill_in :user_email, with: user.email
-#     fill_in :user_password, with: user.password
-#     click_button 'ログイン'
+#     
 #   end
 
-#   describe '新規作成機能' do
-#     context '仕入を新規登録した場合' do
-#       it '仕入の編集画面に選んだ商品が表示される' do
-#         visit new_team_purchase_path(@team)
-#         fill_in 'date_at', with: '2022.02.02'
-#         fill_in :inputter, with: '800'
-        
-#         click_button '登録する'
-#         visit edit_team_product_path
-#         expect(page).to have_content 'abc1234'
-#       end
-#     end
-#   end
+  describe '新規作成機能' do
+    context '仕入を新規登録した場合' do
+      it '登録後の画面で選んだ商品が表示される' do
+        user = FactoryBot.create(:user, :a)
+        @team = FactoryBot.create(:team, user: user)
+        @grouping = Grouping.create(user_id: user.id, team_id: @team.id)
+        @supplier = Supplier.create(name: "サプライヤー")
+        @product = FactoryBot.create(:product, user: user, team: @team)        
+        @second_product = FactoryBot.create(:second_product, user: user, team: @team)
+        visit new_user_session_path
+        fill_in :user_email, with: user.email
+        fill_in :user_password, with: user.password
+        click_button 'ログイン'
+        visit new_team_purchase_path(@team)
+        fill_in :purchase_date_at, with: '002022-02-02'
+        select 'サプライヤー', from: 'purchase[supplier_id]'        
+        click_button '登録する'        
+        expect(page).to have_content '登録しました'
+      end
+    end
+  end
 # #   describe '一覧表示機能' do
 # #     context '一覧画面に遷移した場合' do
 # #       it '作成済みの商品一覧が表示される' do
